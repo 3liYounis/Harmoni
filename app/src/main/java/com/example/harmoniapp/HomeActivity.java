74a11cd12d5b71c55c6dd17a1f6bea5512d0057e
Tamar <tamiyami.sha@gmail.com>
2022-07-21 17:35:52 +0300

    Opened a home page activity and xml file for it (with the main and side activities) with buttons Opened a mission activity and xml for it with text and buttons Connected the mission and home page with a button

---
+++ app/src/main/java/com/example/harmoniapp/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.harmoniapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity implements  View.OnClickListener {
+    Button main_activity, side_activity1, side_activity2, side_activity3, side_activity4;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_current);
+        setContentView(R.layout.activity_home_current);
+        main_activity = findViewById(R.id.main_activity);
+        side_activity1 = findViewById(R.id.side_activity1);
+        side_activity2 = findViewById(R.id.side_activity2);
+        side_activity3 = findViewById(R.id.side_activity3);
+        side_activity4 = findViewById(R.id.side_activity4);
+
+        main_activity.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == main_activity) {
+           Intent MainActivityIntent = new Intent(this, MissionViewActivity.class);
+            startActivity(MainActivityIntent);
+        }
+
+    }
+}
+
