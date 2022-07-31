bdf6630b7363203ba49942437b2d85bd04c3bd9d
Tamar <tamiyami.sha@gmail.com>
2022-07-31 19:39:28 +0300

    Tamar is here !

--- app/src/main/java/com/example/harmoniapp/TestWritingActivity.java
+++ app/src/main/java/com/example/harmoniapp/TestWritingActivity.java
@@ -2,30 +2,35 @@ package com.example.harmoniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.autofill.AutofillValue;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class TestWritingActivity extends AppCompatActivity implements View.OnClickListener {
-RadioButton GoodButton;
-EditText EmotionName;
+    RadioButton GoodButton;
+    EditText EmotionName;
+    TextView SideActivityName, WritingActQuo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_writing);
         GoodButton = findViewById(R.id.GoodButton);
-        //EmotionName = findViewById(R.id.EmotionName);
+        EmotionName = findViewById(R.id.EmotionName);
+        SideActivityName = findViewById(R.id.SideActivityName);
+        WritingActQuo =  findViewById(R.id.WritingActQuo);
 
     }
 
     @Override
     public void onClick(View view) {
         if (view == GoodButton) {
-
+            String GoodButtonId = (String) GoodButton.getText();
+            EmotionName.setText(GoodButtonId);
         }
+
     }
+
 }
